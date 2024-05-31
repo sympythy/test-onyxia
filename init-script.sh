c651ada636c7a9921e4448784ccf7274b70c025e
sympythy <112191660+sympythy@users.noreply.github.com>
2024-05-31 16:30:45 +0200

    Update init-script.sh
    Use $HOME in path to requirements file

--- init-script.sh
+++ init-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install -r ./test-instance/requirements.txt
+pip install -r "$HOME/test-instance/requirements.txt"
 python -m nltk.downloader punkt
 python -m spacy download en_core_web_sm
 
