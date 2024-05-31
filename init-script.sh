9df1ed0f6dcb9a4339bca13c2e4dc00f01f18f0c
sympythy <112191660+sympythy@users.noreply.github.com>
2024-05-31 14:23:02 +0200

    Update init-script.sh
    Add a missing newline

--- init-script.sh
+++ init-script.sh
@@ -1,4 +1,5 @@
-#!/bin/bash pip install -r test-instance/requirements.txt
+#!/bin/bash
+pip install -r test-instance/requirements.txt
 python -m nltk.downloader punkt
 python -m spacy download en_core_web_sm
 
