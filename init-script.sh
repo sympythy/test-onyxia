0a9799070cc433336ce953d69e09cac69167f53e
sympythy <112191660+sympythy@users.noreply.github.com>
2024-05-31 16:37:55 +0200

    Update init-script.sh
    Use another environment variable: GIT_REPOSITORY

--- init-script.sh
+++ init-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install -r "$HOME/test-instance/requirements.txt"
+pip install -r "$GIT_REPOSITORY/requirements.txt"
 python -m nltk.downloader punkt
 python -m spacy download en_core_web_sm
 
