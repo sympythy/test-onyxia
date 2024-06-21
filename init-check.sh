3d2ac90e1582d0ba934cfc4969868498e4a0f822
sympythy <112191660+sympythy@users.noreply.github.com>
2024-06-21 11:18:18 +0200

    Update init-check.sh
    Test missing directory

--- init-check.sh
+++ init-check.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ ! -d "$WORKSPACE_DIR/test-instance2" ]; then
+  "$DIRECTORY not found -> FAIL" > check.log
+  exit 1
+fi
+
 pip install GPUtil
 python "$WORKSPACE_DIR/test-instance/check_instance.py"
