15455e236855747633eb1be4af5bd3a3e221d6bf
sympythy <112191660+sympythy@users.noreply.github.com>
2024-06-21 14:25:04 +0200

    Update init-check.sh
    Documentation + set real git repository

--- init-check.sh
+++ init-check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-OUTPUT_DIR="$WORKSPACE_DIR/test-instance2"
+# Verification de configuration : GPU, acces au depot git...
+
+# prerequis : configurer le depot Git contenant le script 'check_instance.py' (depot test-instance)
+# "test-instance" correspond au depot Git (a adapter)
+OUTPUT_DIR="$WORKSPACE_DIR/test-instance"
 if [ ! -d "$OUTPUT_DIR" ]; then
   echo "$OUTPUT_DIR not found -> FAIL" > check.log
   exit 1
